6f7d014b225f8609f4a6a31ba5a9aa762567b347
Priscila Patricio <priscilapatricio2@gmail.com>
2021-02-09 21:58:21 -0300

    Inserindo Booststrap e Fontawesome

--- core/views/layouts/html_header.php
+++ core/views/layouts/html_header.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    
+
 </head>
 <body>
   
--- core/views/pagina_inicial.php
+++ core/views/pagina_inicial.php
@@ -1,7 +1,10 @@
-<h1><?= $titulo ?></h1>
+<h1>Texto</h1>
+<h2>Texto</h2>
+<h3>Texto</h3>
+<h4>Texto</h4>
+<h5>Texto</h5>
+<h6>Texto</h6>
 
-<ul>
-<?php foreach ($clientes as $cliente):?>
-    <li><?= $cliente ?></li>
-<?php endforeach;?>
-</ul>
+<h1><i class="far fa-trash-alt"></i></h1>
+
+
